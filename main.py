70026b9f95fb149123fdef9804c4a4f3699cd3a4
Najmul Huda <najmul.myself@gmail.com>
2021-09-16 23:49:47 +0600

    timer mechanism added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-
+from tkinter import * 
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -12,12 +13,28 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    countdown(5 * 60)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
+def countdown(count):
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+
+    canvas.itemconfig(timer_text , text=f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000 , countdown , count - 1)
+        # This is the method to work with gui | after is something like await;
+
+        #Now we want to show this time in our canvas | to do this we need to assign variable what we want to show
+        # in this case we want this in canvas text( text in tomato) | lets create a variable named timer_text
+        # creating a variable is not ultimate solution because we cant show this in screen with our old method (variable.config) 
+        #we need to use canvas.itemconfig() | first argument is where we want to configure and second is what we want to  change
+
+
 # ---------------------------- UI SETUP ------------------------------- #
 
-from tkinter import * 
 
 window = Tk()
 window.title("Promodoro")
@@ -25,11 +42,11 @@ window.config(padx=100 , pady=50 , bg=YELLOW )
 # when we change the background color of the window it shows a problem of images background
 #need to change image background 
 
+
 canvas_title = Label(text="Timer" , font=(FONT_NAME , 50) , bg=YELLOW , fg=GREEN)
 canvas_title.grid(column=1 , row=0)
 
-def update(*arg):
-    pass
+
 
 canvas = Canvas(width=200, height=224 , bg=YELLOW , highlightthickness= 0)
 #When we change the image background another problem aries that image has a border and its white .
@@ -40,11 +57,11 @@ canvas = Canvas(width=200, height=224 , bg=YELLOW , highlightthickness= 0)
 tomato_img = PhotoImage(file = "tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 # cordination selected bcz we exactly want img to be centered
-canvas.create_text(110 , 130, text="00:00" , fill="white" , font=(FONT_NAME , 35 , "bold"))
+timer_text = canvas.create_text(110 , 130, text="00:00" , fill="white" , font=(FONT_NAME , 35 , "bold"))
 canvas.grid(column=1 , row=1)
 
 
-start_btn = Button(text="Start" , highlightthickness=0 , padx=10)
+start_btn = Button(text="Start" , highlightthickness=0 , padx=10 , command=start_timer)
 start_btn.grid(column=0 , row=3)
 reset_btn = Button(text="Reset" , highlightthickness=0 , padx=10)
 reset_btn.grid(column=2 , row=3)
